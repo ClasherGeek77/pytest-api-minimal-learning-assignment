1511a1253660d8d1f6d5e0924571a2874aae2017
Maissy <maissy.tsara@julofinance.com>
2024-02-29 13:32:28 +0700

    Maissy_API_Exercise

--- api/tests/test_delete_users.py
+++ api/tests/test_delete_users.py
@@ -2,19 +2,19 @@ import logging
 import requests
 import json
 
-def test_login_success_reqres(secrets_login):
+def test_delete_users_success_reqres(secrets_delete_users):
     
-    url = "https://reqres.in/api/login"
+    url = "https://reqres.in/api/users"
     payload = {
-        "email" : secrets_login.email,
-        "password" : secrets_login.password
+        "email" : secrets_delete_users.name,
+        "password" : secrets_delete_users.job
     }
     headers = {
         "Content-Type": "application/json"
     }
     response = requests.post(url, headers=headers, json=payload)
     
-    assert response.status_code == 200
+    assert response.status_code == 201
 
     logging.info("API Response: ")
     logging.info(response.content.decode("utf-8"))
